deb7f6ca0079829a210e44233dbb46787fca4dc8
solomonfy <fantahunsolomon@gmail.com>
2020-09-05 13:43:07 -0400

    edit delete icons done for prescription

--- back-end-prescription-tracker/app/controllers/api/v1/prescriptions_controller.rb
+++ back-end-prescription-tracker/app/controllers/api/v1/prescriptions_controller.rb
@@ -6,5 +6,10 @@ class Api::V1::PrescriptionsController < ApplicationController
         render json: @prescriptions, include: [:user, :medication]
     end
 
+    def show
+        # byebug
+        @prescription = Prescription.find_by(id: params[:id])
+        render json: @prescription, include: [:user, :medication]
+    end
     
 end
