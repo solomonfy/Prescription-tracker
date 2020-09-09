f68cae37a73f70b44c81fc7934e55407bf58c8bb
solomonfy <fantahunsolomon@gmail.com>
2020-09-09 11:56:17 -0400

    trying to merge

--- back-end-prescription-tracker/app/serializers/prescription_serializer.rb
+++ back-end-prescription-tracker/app/serializers/prescription_serializer.rb
@@ -1,5 +1,5 @@
 class PrescriptionSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :medication_name, :medication_imprint, :medication_strength, :medication_category, :medication_precaution, :medication_image, :unique_id, :frequency, :dose, :time_to_take, :date_prescribed, :doctor_name
+  attributes :id, :user_id, :medication_name, :medication_imprint, :medication_strength, :medication_category, :medication_precaution, :medication_image, :unique_id, :frequency, :dose, :time_to_take, :prescription_taken, :date_prescribed, :doctor_name
 
   belongs_to :user
 end
--- front-end-prescription-tracker/index.js
+++ front-end-prescription-tracker/index.js
@@ -39,7 +39,7 @@ function fetchData() {
 
 function renderAllPrescriptions(prescriptions) {
   for (const prescription of prescriptions) {
-    dipslayPrescription(prescription);
+    displayPrescription(prescription);
   }
 }
 
@@ -65,7 +65,7 @@ if (3 > 2) {
   missedUl.append(missedLi);
 }
 
-function dipslayPrescription(prescription) {
+function displayPrescription(prescription) {
   const medLi = document.createElement("li");
   medUl.append(medLi);
 
@@ -78,20 +78,20 @@ function dipslayPrescription(prescription) {
   // condition to show the prescription on "Medications to be taken on" or "Medication taken" side
 
   if (prescription.prescription_taken === false) {
-    
-    medNameP.innerText = prescription.medication.name + " ";
-    medStrengthSpan.innerText = prescription.medication.strength + " ";
+
+    medNameP.innerText = prescription.medication_name + " ";
+    medStrengthSpan.innerText = prescription.medication_strength + " ";
     medTimeSpan.innerText = prescription.time_to_take;
-    
-    const btnDiv = document.createElement("div");
-    btnDiv.className = "list-item-menu";
-    
+
+    // const btnDiv = document.createElement("div");
+    // btnDiv.className = "list-item-menu";
+
     // bell, delete and edit icons
     const checkTag = document.createElement("a");
     checkTag.innerHTML = `<a class="check-button" uk-icon="icon: bell; ratio: 2"></a>`;
-      checkTag.addEventListener("click", () => {
-        medLi.innerHTML = "";
-        fetch(`${url}${prescription.id}`, {
+    checkTag.addEventListener("click", () => {
+      medLi.innerHTML = "";
+      fetch(`${url}${prescription.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
@@ -102,33 +102,33 @@ function dipslayPrescription(prescription) {
           })
         })
         .then((response => response.json()))
-        .then((prescription) => dipslayPrescription(prescription))
-      })
- 
+        .then((prescription) => displayPrescription(prescription))
+    })
+
     const editATag = document.createElement("a");
     editATag.innerHTML = `<a class="edit-button" uk-icon="icon: pencil" uk-tooltip="Edit prescription"></a>`;
-    
+
     const deleteATag = document.createElement("a");
     deleteATag.innerHTML = `<a class="delete-button" uk-icon="icon: trash" uk-tooltip="Delete prescription"></a>`;
-    
+
     medNamDiv.append(medNameP, medStrengthSpan, medTimeSpan, breakTag);
     medLi.append(medNamDiv, checkTag, editATag, deleteATag);
 
-    
+
     //-------------- end of if medication_taken === false
 
   } else {
 
     const medTakenUl = document.querySelector("ul.taken-medication");
     const medTakenLi = document.createElement("li");
-            
+
     medTakenUl.append(medTakenLi);
-    medTakenLi.innerText = prescription.medication.name;
+    medTakenLi.innerText = prescription.medication_name;
 
-  }  //------------- end of if medication_taken === true
+  } //------------- end of if medication_taken === true
 
-     //---- send all prescriptions to "Medications to be taken on" -----//
-     
+  //---- send all prescriptions to "Medications to be taken on" -----//
+
   medNamDiv.addEventListener("click", () => {
     const containerDiv = document.createElement("div");
     containerDiv.innerHTML = "";
@@ -143,12 +143,12 @@ function dipslayPrescription(prescription) {
 
     medNameTag.innerText =
       "Name: " +
-      prescription.medication.name +
+      prescription.medication_name.name +
       " " +
-      prescription.medication.strength;
-    medImprintTag.innerText = "Imprint: " + prescription.medication.imprint;
+      prescription.medication_strength;
+    medImprintTag.innerText = "Imprint: " + prescription.medication_imprint;
 
-    medImage.src = prescription.medication.image;
+    medImage.src = prescription.medication_image;
 
     polaroidDiv.append(medImage, medImprintTag, medNameTag);
     containerDiv.append(polaroidDiv);
@@ -168,31 +168,27 @@ medReset.addEventListener("click", () => {
     .then((response) => response.json())
     .then((prescriptions) => updatePrescriptionTaken(prescriptions))
 })
-  
-  
+
+
 function updatePrescriptionTaken(prescriptions) {
-  prescriptions.forEach(prescription => makeFalse(prescription)) 
+  prescriptions.forEach(prescription => makeFalse(prescription))
 }
 
 
 //--------- change "prescription_taken" to "false" --------------------//
 function makeFalse(prescription) {
-  setTimeout(function() {                  // add delay in loop
+  setTimeout(function () { // add delay in loop
     fetch(`${url}${prescription.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({
-        prescription_taken: false
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          prescription_taken: false
+        })
       })
-    })
       .then((response) => response.json())
-      .then((prescription) => dipslayPrescription(prescription))
+      .then((prescription) => displayPrescription(prescription))
   }, 100 * prescription.id);
 };
-
-
-
-// console.log(new Date().toLocaleTimeString());
