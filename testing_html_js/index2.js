4f966dd4bd2955ecd3afa8eb06cd80b235516755
solomonfy <fantahunsolomon@gmail.com>
2020-09-11 13:05:23 -0400

    done

--- testing_html_js/index2.js
+++ testing_html_js/index2.js
@@ -1,25 +1,341 @@
-// $("#search-criteria").on("keyup", function () {
-//     var g = $(this).val().toLowerCase();
-//     $(".fbbox .fix label").each(function () {
-//         var s = $(this).text().toLowerCase();
-//         $(this).closest('.fbbox')[s.indexOf(g) !== -1 ? 'show' : 'hide']();
-//     });
-// });​
+// Display today's date
+const todayDateTag = document.getElementById("spanDate");
+todayDateTag.innerHTML = new Date().toLocaleDateString();
 
-// Get medication list from API
+const todayTimeTag = document.getElementById("currentTime");
+let currentTime = setInterval(timer, 1000);
 
-fetch("https://api.fda.gov/drug/drugsfda.json?limit=1000")
-    .then(resp => resp.json())
-    .then(data => renderAllProducts(data.results))
-
-function renderAllProducts(data) {
-    data.forEach(element => {
-        getEachProduct(element.products)
-    });
+function timer() {
+  todayTimeTag.innerHTML = new Date().toLocaleTimeString();
 }
 
-function getEachProduct(products) {
-    products.forEach(product => {
-        console.log(product.brand_name)
+const divMissedMed = document.querySelector("div.missed-medications");
+const url = "http://localhost:3000/api/v1/prescriptions/";
+
+const addBtn = document.querySelector("a.add-button");
+const prescriptionFormContainer = document.getElementById(
+  "prescription-form-container"
+);
+// const editPrescriptionForm = document.querySelector("#edit-prescription-container");
+const editPrescriptionForm = document.querySelector(
+  "form#edit-prescription-form"
+);
+
+//create hidden Id field for edit form
+let hiddenId = document.createElement("input");
+hiddenId.setAttribute("type", "hidden");
+editPrescriptionForm.append(hiddenId);
+
+const medDetailDiv = document.querySelector(".medication-detail");
+const medDiv = document.querySelector("div#medication-detail")
+// console.log(medDiv)
+const medUl = document.querySelector("ul#medication-list-ul");
+const refreshBtn = document.querySelector("a.refresh-button");
+const breakTag = document.createElement("br");
+addPrescription = false;
+const wellDoneTag = document.querySelector("div.done-for-today")
+// let allPrescriptions = [];
+
+addBtn.addEventListener("click", () => {
+  addPrescription = !addPrescription;
+  if (addPrescription) {
+    prescriptionFormContainer.style.display = "block";
+  } else {
+    prescriptionFormContainer.style.display = "none";
+  }
+
+  // POST - add new prescription
+
+  prescriptionFormContainer.addEventListener("submit", () => {
+    event.preventDefault;
+
+    let medication_name = event.target[0].value;
+    let medication_strength = event.target[1].value;
+    let medication_imprint = event.target[2].value;
+    let medication_precaution = event.target[3].value;
+    let medication_category = event.target[4].value;
+    let medication_image = event.target[5].value;
+    let frequency = event.target[6].value;
+    let dose = event.target[7].value;
+    let time_to_take = event.target[8].value;
+
+    configObj = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: 1,
+        medication_name,
+        medication_strength,
+        medication_imprint,
+        medication_precaution,
+        medication_category,
+        medication_image,
+        frequency,
+        dose,
+        time_to_take,
+      }),
+    };
+    fetch(url, configObj)
+      .then((resp) => resp.json())
+      .then((newPrescription) => displayPrescription(newPrescription));
+
+    prescriptionFormContainer.reset();
+  });
+});
+
+fetchData();
+let fetchTime = setInterval(fetchData, 60000);
+
+function fetchData() {
+  medUl.innerHTML = "";
+  const medTakenUl = document.querySelector("ul.taken-medication");
+  medTakenUl.innerHTML = "";
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => renderAllPrescriptions(data));
+}
+
+function renderAllPrescriptions(prescriptions) {
+
+  const checkPrescription = (prescription => prescription.prescription_taken === true);
+  if (prescriptions.every(checkPrescription)) {
+    wellDoneTag.style.display = "block"
+    wellDoneTag.innerText = "You are done for today: click refresh to set for tomorrow"
+    document.querySelector("#time-for-medication-p-tag").innerHTML = ""
+  }
+
+  for (const prescription of prescriptions) {
+    displayPrescription(prescription);
+  }
+
+}
+
+
+
+function displayPrescription(prescription) {
+  const medLi = document.createElement("li");
+
+  // Missed medications alert
+
+  let currentHour = new Date().getHours();
+  let currentMinutes = new Date().getMinutes();
+  let currentHourMin = `${currentHour}:${currentMinutes}`;
+
+  let medTime = timeToDecimal(prescription.time_to_take);
+  let currHourMin = timeToDecimal(currentHourMin);
+
+  function timeToDecimal(t) {
+    var arr = t.split(":");
+    var dec = parseInt((arr[1] / 6) * 10, 10);
+    return parseFloat(parseInt(arr[0], 10) + "." + (dec < 10 ? "0" : "" + dec));
+  }
+
+  if (medTime < currHourMin) {
+    medLi.className = "uk-alert-danger";
+  }
+  medUl.append(medLi);
+
+  const medNamDiv = document.createElement("div");
+  const medNameP = document.createElement("strong");
+  const medStrengthSpan = document.createElement("span");
+  const medTimeSpan = document.createElement("span");
+  medTimeSpan.className = "med-time";
+
+  // condition to show the prescription on "Medications to be taken on" or "Medication taken" side
+
+  if (prescription.prescription_taken === false) {
+    medNameP.innerText = prescription.medication_name + " ";
+    medStrengthSpan.innerText = prescription.medication_strength + " ";
+    medTimeSpan.innerText = prescription.time_to_take;
+
+    // bell, delete and edit icons
+    const checkTag = document.createElement("a");
+    checkTag.innerHTML = `<a class="check-button" uk-icon="icon: unlock; ratio: 2" uk-tooltip="Took my prescription"></a>`;
+    checkTag.addEventListener("click", () => {
+      medLi.innerHTML = "";
+      fetch(`${url}${prescription.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            prescription_taken: true,
+          }),
+        })
+        .then((response) => response.json())
+        .then((prescription) => displayPrescription(prescription));
     });
+
+    const editATag = document.createElement("a");
+    editATag.innerHTML = `<a class="edit-button" uk-icon="icon: pencil" uk-tooltip="Edit prescription" uk-toggle="target: #edit-prescription-container"></a>`;
+
+    //click on edit button to populate form
+    editATag.addEventListener("click", () => {
+      editPrescriptionForm.children[0].value = prescription.frequency;
+      editPrescriptionForm.children[1].value = prescription.dose;
+      editPrescriptionForm.children[2].value = prescription.time_to_take;
+      editPrescriptionForm.children[3].value = prescription.medication_imprint;
+      editPrescriptionForm.children[4].value = prescription.medication_image;
+      // hiddenId = prescription.id;
+
+      // edit prescription
+      document.querySelector("input.frequency").value = prescription.frequency;
+      document.querySelector("input.dose").value = prescription.dose;
+      document.querySelector("input.time").value = prescription.time_to_take;
+      document.querySelector("input.imprint").value = prescription.medication_imprint;
+
+      editPrescriptionForm.addEventListener("submit", () => {
+        // event.preventDefault();
+
+        let med_frequency = event.target[0].value;
+        let med_dose = event.target[1].value;
+        let med_time_to_take = event.target[2].value;
+        let med_imprint = event.target[3].value;
+        let med_image = event.target[4].value;
+
+        configObj = {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            frequency: med_frequency,
+            dose: med_dose,
+            time_to_take: med_time_to_take,
+            medication_imprint: med_imprint,
+            medication_image: med_image,
+          }),
+        };
+
+        fetch(url + prescription.id, configObj)
+          .then((resp) => resp.json())
+          .then((updatedPrescription) =>
+            displayPrescription(updatedPrescription)
+          );
+        editPrescriptionForm.reset();
+      });
+    });
+
+    // Delete prescription
+    const deleteATag = document.createElement("a");
+    deleteATag.innerHTML = `<a class="delete-button" uk-icon="icon: trash" uk-tooltip="Delete prescription"></a>`;
+
+    deleteATag.addEventListener("click", () => {
+      fetch(url + prescription.id, {
+        method: "DELETE",
+      });
+      medLi.innerText = "";
+    });
+
+    medNamDiv.append(medNameP, medStrengthSpan, medTimeSpan, breakTag);
+    medLi.append(medNamDiv, checkTag, editATag, deleteATag);
+
+    //-------------- end of if medication_taken === false
+  } else {
+    const medTakenUl = document.querySelector("ul.taken-medication");
+    const h3 = document.createElement("h3");
+    const medTakenLi = document.createElement("p");
+    medTakenLi.className = "uk-text-success uk-text-large"
+
+    medTakenUl.append(medTakenLi);
+    // medTakenUl.append(h3);
+    // h3.append(medTakenLi);
+    medTakenLi.innerText =
+      prescription.medication_name + " " + prescription.medication_strength;
+  } //------------- end of if medication_taken === true
+
+  //---- send all prescriptions to "Medications to be taken on" -----//
+
+  const containerDiv = document.createElement("div");
+
+  medNamDiv.addEventListener("click", () => {
+
+    medDiv.innerHTML = "";
+    medDiv.append(containerDiv);
+    containerDiv.innerHTML = "";
+    const polaroidDiv = document.createElement("div");
+    polaroidDiv.className = "polaroid";
+
+    polaroidDiv.setAttribute("uk-alert", "");
+
+    containerDiv.append(polaroidDiv);
+
+    const medNameTag = document.createElement("h2");
+    const medImprintTag = document.createElement("h2");
+    const medImage = document.createElement("img");
+    medImage.className = "medication-image";
+    const medPrecauTag = document.createElement("h2");
+    const medFrequencyAndDoseTag = document.createElement("h2");
+
+    const closeButton = document.createElement("a");
+    closeButton.classList.add("uk-alert-close", "uk-close-large");
+    closeButton.id = "close-btn"
+    closeButton.setAttribute("uk-close", "");
+
+    medNameTag.innerText =
+      prescription.medication_name + " " + prescription.medication_strength;
+    medImprintTag.innerText = "Imprint: " + prescription.medication_imprint;
+
+    medImage.src = prescription.medication_image;
+    medPrecauTag.innerText = prescription.medication_precaution;
+
+    medFrequencyAndDoseTag.innerText =
+      "You should take this medication " +
+      prescription.dose +
+      " " +
+      prescription.medication_category +
+      " " +
+      prescription.frequency;
+
+    polaroidDiv.append(
+
+      closeButton,
+      breakTag,
+      breakTag,
+      medImage,
+      medImprintTag,
+      medNameTag,
+      medPrecauTag,
+      medFrequencyAndDoseTag
+    );
+    // containerDiv.append(medNameTag, medImprintTag, medImage, medPrecauTag);
+  });
+}
+
+const medReset = document.querySelector("a.reset-button");
+
+medReset.addEventListener("click", () => {
+  medUl.innerHTML = "";
+  const medTakenUl = document.querySelector("ul.taken-medication");
+  medTakenUl.innerHTML = "";
+  fetch(url)
+    .then((response) => response.json())
+    .then((prescriptions) => updatePrescriptionTaken(prescriptions));
+});
+
+function updatePrescriptionTaken(prescriptions) {
+  prescriptions.forEach((prescription) => makeFalse(prescription));
+}
+
+//--------- change "prescription_taken" to "false" --------------------//
+function makeFalse(prescription) {
+  setTimeout(function () {
+    // add delay in loop
+    fetch(`${url}${prescription.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          prescription_taken: false,
+        }),
+      })
+      .then((response) => response.json())
+      .then((prescription) => displayPrescription(prescription));
+  }, 100 * prescription.id);
 }
